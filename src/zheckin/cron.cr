de1a86697fbd6021ee653ad9667bc7e2ab6dc079
Hentioe <me@bluerain.io>
2019-12-28 08:01:23 +0800

    Add checkin log

--- src/zheckin/cron.cr
+++ src/zheckin/cron.cr
@@ -7,10 +7,8 @@ module Zheckin::Cron
 
   def self.init
     # 将固定的北京时间转换为本地时间
-    beijing_location = Time::Location.fixed(3600 * 8)
-    beijing_time = Time.local(1970, 1, 1, SCHEDULE_HOUR, SCHEDULE_MINUTE, 0, location: beijing_location)
-    date_time = beijing_time.to_local_in(Time.local.location)
-    time_s = date_time.to_s("%H:%M:%S")
+    time = Time.local(1970, 1, 1, SCHEDULE_HOUR, SCHEDULE_MINUTE, 0)
+    time_s = time.to_s("%H:%M:%S")
 
     Logging.info "scheduling time: #{time_s}"
 
@@ -18,11 +16,15 @@ module Zheckin::Cron
   end
 
   private def self.start
+    Logging.info "checkin started"
+
     # 获取个人资料并刷新圈子列表
     account = Zhihu::WrapperApi.self(SELF_API_TOKEN)
     clubs = Zhihu::WrapperApi.clubs_joined(account)
     Store.refresh_account_clubs!(account, clubs)
     # 开始签到
     Zhihu::WrapperApi.clubs_checkin_all(account)
+
+    Logging.info "checkin done"
   end
 end
