d1654c7b89f02ead8fa8353e1c32b40f31e0718d
Hentioe <me@bluerain.io>
2020-01-05 21:39:07 +0800

    Add header/main layout

--- assets/js/console/components/PageContainer.js
+++ assets/js/console/components/PageContainer.js
@@ -1,6 +1,12 @@
 import React from "react";
 
+import Header from "./Header";
+
 export default ({ children }) => {
-  console.log(children);
-  return <div>{children}</div>;
+  return (
+    <div className="flex md:px-20 lg:px-48 xl:px-64">
+      <Header className="bg-red-200 w-3/12 p-2" />
+      <main className="bg-gray-200 w-9/12 p-2">{children}</main>
+    </div>
+  );
 };
